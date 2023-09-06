a41cef14e3f2b8f4368339a34a7c7d824a95ac05
shenwang1234 <shen1091035694@gmail.com>
2023-09-06 15:12:20 -0400

    cleaned playgnd.c

--- playgnd.c
+++ playgnd.c
@@ -13,6 +13,7 @@ major
  1. minimax
  2. pruning
 2.ui
+3.algorithm. it doesnt take immediate win cuz score system
 
 Try to break the game
 - crash
@@ -20,60 +21,53 @@ Try to break the game
 
  
 */
-#define Player1 'x'
-#define Player2 'o'
-
-
-
 int isValid(int positionNum, board* gameBoard);//check if posotion entered is available 
 int getInput(board* gameBoard);//has to be integer
 void pveStartGame();
 
 int main(){
-
 	pveStartGame();
-
 }
 
-
+//take a board and position as input. 
+// return 1 if valid 
 int isValid(int positionNum, board* gameBoard){
-	if (positionNum>0 && positionNum<10){
-		if(gameBoard->boardPositions[positionNum]!=Player1 &&gameBoard->boardPositions[positionNum]!=Player2) return 1;
+	if (positionNum>0 && positionNum<10){ //check if position is within boundaries
+		if(gameBoard->boardPositions[positionNum]!=Player1 &&gameBoard->boardPositions[positionNum]!=Player2) return 1;//check if position is occupied
 		else return -1;
 	}else return -1;
 }
+
+//get input from cmd. check if input is valid.
+//if it is return
+//else ask user to input again 
 int getInput(board* gameBoard){
 	int position = 0;
 	int statusOfScan = 0;
+	printf("\n\tenter the position number of your choice: ");
 	while(1){
 		do{
-			printf("\t");
 			statusOfScan = scanf("%d",&position); //try getInput(), scanf ignores leading white space
 			if(statusOfScan!=1){
 				int c;
 				while ((c = getchar()) != '\n' && c != EOF){} //clear buffer for scanf
-	            printf("Invalid input. Please enter a number: ");
-		}
+	            printf("\n\tInvalid input. Please enter a number: ");
+			}
 		}while(statusOfScan!=1);
-			if(isValid(position, gameBoard)!=1) {
-				printf("position unable. Please enter a number: ");
-				continue;
-			}else return position;
+		if(isValid(position, gameBoard)!=1) {
+			printf("\n\tposition unable. Please enter a number: ");
+			continue;
+		}else return position;
 	}
-	
-
 }
 
 //return 2 if player 2 won, 1 if player 1 won, -1 if n
-
-
 void pveStartGame(){
 	board* gameboard = initBoard();
 	int position = 0;
 	int gameOnGoing = 1;
 	int isTie = 0;
 	printf("\n\n\n\tGame Start!\n\n\n");
-	printf("\tenter the position number of your choice\n");
 	printBoard(gameboard);
 	while(gameOnGoing && !isTie){
 		printf("\tplayer 1 go\n");
@@ -101,7 +95,3 @@ void pveStartGame(){
 		}
 	}
 }
-
-
-
-
